ff72829045147fc21453637c1c9dbb6b67e39acb
vignesh <vigneshm347@gmail.com>
2018-08-06 16:28:59 -0400

    First commit - Admin views directory structure is created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.err_master')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h2 style="text-align: center">
+                Oops! 404 Error! <hr>
+                <small>
+                    Please go back
+                </small>
+            </h2>
+        </div>
+    </div>
+
+    @endsection
